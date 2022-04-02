e3d855d2bc8618e409877e0cfb1eedc73b1be59d
djzhang <trujunzhang@gmail.com>
2022-04-02 08:56:05 +0800

    xxx

--- lib/shop/page/shop_page_BAK.dart
+++ lib/shop/page/shop_page_BAK.dart
@@ -58,141 +58,138 @@ class _ShopPageState extends State<ShopPage>
       margin: const EdgeInsets.only(left: 16.0),
       child: Gaps.line,
     );
-    return ChangeNotifierProvider<UserProvider>(
-      create: (_) => provider,
-      child: Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(
-              tooltip: '消息',
-              onPressed: () {
-                NavigatorUtils.push(context, ShopRouter.messagePage);
-              },
-              icon: LoadAssetImage(
-                'shop/message',
-                key: const Key('message'),
-                width: 24.0,
-                height: 24.0,
-                color: _iconColor,
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            tooltip: '消息',
+            onPressed: () {
+              NavigatorUtils.push(context, ShopRouter.messagePage);
+            },
+            icon: LoadAssetImage(
+              'shop/message',
+              key: const Key('message'),
+              width: 24.0,
+              height: 24.0,
+              color: _iconColor,
             ),
-            IconButton(
-              tooltip: '设置',
-              onPressed: () {
-                NavigatorUtils.push(context, SettingRouter.settingPage);
-              },
-              icon: LoadAssetImage(
-                'shop/setting',
-                key: const Key('setting'),
-                width: 24.0,
-                height: 24.0,
-                color: _iconColor,
-              ),
-            )
-          ],
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Gaps.vGap12,
-            Consumer<UserProvider>(
-              builder: (_, provider, child) {
-                final Widget header = Stack(
-                  children: <Widget>[
-                    const SizedBox(width: double.infinity, height: 56.0),
-                    const Text(
-                      '官方直营店',
-                      style: AppTextStyles.textBold24,
-                    ),
-                    Positioned(
-                      right: 0.0,
-                      child: CircleAvatar(
-                        radius: 28.0,
-                        backgroundColor: Colors.transparent,
-                        backgroundImage: ImageUtils.getImageProvider(
-                            provider.user?.avatarUrl,
-                            holderImg: 'shop/tx'),
-                      ),
-                    ),
-                    child!,
-                  ],
-                );
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: MergeSemantics(
-                    child: header,
+          ),
+          IconButton(
+            tooltip: '设置',
+            onPressed: () {
+              NavigatorUtils.push(context, SettingRouter.settingPage);
+            },
+            icon: LoadAssetImage(
+              'shop/setting',
+              key: const Key('setting'),
+              width: 24.0,
+              height: 24.0,
+              color: _iconColor,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Gaps.vGap12,
+          Consumer<UserProvider>(
+            builder: (_, provider, child) {
+              final Widget header = Stack(
+                children: <Widget>[
+                  const SizedBox(width: double.infinity, height: 56.0),
+                  const Text(
+                    '官方直营店',
+                    style: AppTextStyles.textBold24,
                   ),
-                );
-              },
-              child: Positioned(
-                top: 38.0,
-                left: 0.0,
-                child: Row(
-                  children: const <Widget>[
-                    LoadAssetImage(
-                      'shop/zybq',
-                      width: 40.0,
-                      height: 16.0,
+                  Positioned(
+                    right: 0.0,
+                    child: CircleAvatar(
+                      radius: 28.0,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: ImageUtils.getImageProvider(
+                          provider.user?.avatarUrl,
+                          holderImg: 'shop/tx'),
                     ),
-                    Gaps.hGap8,
-                    Text('店铺账号:15000000000', style: AppTextStyles.textSize12)
-                  ],
+                  ),
+                  child!,
+                ],
+              );
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: MergeSemantics(
+                  child: header,
                 ),
+              );
+            },
+            child: Positioned(
+              top: 38.0,
+              left: 0.0,
+              child: Row(
+                children: const <Widget>[
+                  LoadAssetImage(
+                    'shop/zybq',
+                    width: 40.0,
+                    height: 16.0,
+                  ),
+                  Gaps.hGap8,
+                  Text('店铺账号:15000000000', style: AppTextStyles.textSize12)
+                ],
               ),
             ),
-            Gaps.vGap24,
-            line,
-            Gaps.vGap24,
-            const MergeSemantics(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  '账户',
-                  style: AppTextStyles.textBold18,
-                ),
+          ),
+          Gaps.vGap24,
+          line,
+          Gaps.vGap24,
+          const MergeSemantics(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                '账户',
+                style: AppTextStyles.textBold18,
               ),
             ),
-            ShopFunctionModule(
-              data: _menuTitle,
-              image: _menuImage,
-              darkImage: _menuDarkImage,
+          ),
+          ShopFunctionModule(
+            data: _menuTitle,
+            image: _menuImage,
+            darkImage: _menuDarkImage,
+            onItemClick: (index) {
+              if (index == 0) {
+                NavigatorUtils.push(
+                    context, AccountRouter.accountRecordListPage);
+              } else if (index == 1) {
+                NavigatorUtils.push(context, AccountRouter.accountPage);
+              } else if (index == 2) {
+                NavigatorUtils.push(
+                    context, AccountRouter.withdrawalAccountPage);
+              }
+            },
+          ),
+          line,
+          Gaps.vGap24,
+          const MergeSemantics(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                '店铺',
+                style: AppTextStyles.textBold18,
+              ),
+            ),
+          ),
+
+          /// 使用Flexible防止溢出
+          Flexible(
+            child: ShopFunctionModule(
+              data: const ['店铺设置'],
+              image: const ['dpsz'],
+              darkImage: const ['dark_dpsz'],
               onItemClick: (index) {
-                if (index == 0) {
-                  NavigatorUtils.push(
-                      context, AccountRouter.accountRecordListPage);
-                } else if (index == 1) {
-                  NavigatorUtils.push(context, AccountRouter.accountPage);
-                } else if (index == 2) {
-                  NavigatorUtils.push(
-                      context, AccountRouter.withdrawalAccountPage);
-                }
+                NavigatorUtils.push(context, ShopRouter.shopSettingPage);
               },
             ),
-            line,
-            Gaps.vGap24,
-            const MergeSemantics(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  '店铺',
-                  style: AppTextStyles.textBold18,
-                ),
-              ),
-            ),
-
-            /// 使用Flexible防止溢出
-            Flexible(
-              child: ShopFunctionModule(
-                data: const ['店铺设置'],
-                image: const ['dpsz'],
-                darkImage: const ['dark_dpsz'],
-                onItemClick: (index) {
-                  NavigatorUtils.push(context, ShopRouter.shopSettingPage);
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
